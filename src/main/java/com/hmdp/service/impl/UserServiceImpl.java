9567261ecf0672934053acc5e9dd4166eb20519e
zzd <zhaizhidong2021@163.com>
2023-02-25 17:53:10 +0800

    Initial hm-dianping

---
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -0,0 +1,36 @@
+package com.hmdp.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.Result;
+import com.hmdp.entity.User;
+import com.hmdp.mapper.UserMapper;
+import com.hmdp.service.IUserService;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
+
+/**
+ * <p>
+ * 服务实现类
+ * </p>
+ *
+ * @author 虎哥
+ * @since 2021-12-22
+ */
+@Service
+public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
+
+    @Override
+    public Result sendCode(String phone, HttpSession session) {
+        //1.校验手机号
+
+        //2.不符合，直接返回错误信息
+
+        //3，符合，申城返回值
+
+        //4.保存验证码到session
+
+        //5.发送验证码
+        return Result.ok();
+    }
+}
